5bf4ec3db44951b1864a5e251f26f02b8d7e2f93
Daniel Priddle <mac@Dans-MacBook-Pro.local>
2018-05-17 16:55:29 -0400

    first commit

---
+++ InfoCollectPOC/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.DeviceInfo;
+
+namespace InfoCollectPOC
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+			GetDeviceInfo();
+            
+		}
+
+		void GetDeviceInfo()
+		{
+			lblID.Text += CrossDeviceInfo.Current.Id;
+			lblIdiom.Text += CrossDeviceInfo.Current.Idiom.ToString();
+			lblModel.Text += CrossDeviceInfo.Current.Model;
+			lblPlatform.Text += CrossDeviceInfo.Current.Platform;
+			lblVersion.Text += CrossDeviceInfo.Current.Version;
+			lblVersionNum.Text += CrossDeviceInfo.Current.VersionNumber;
+			lblIsDevice.Text += CrossDeviceInfo.Current.IsDevice;
+		}
+	}
+}
